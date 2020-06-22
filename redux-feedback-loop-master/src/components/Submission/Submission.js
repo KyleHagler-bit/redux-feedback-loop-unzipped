07bec0f7e485c57cae03639eed29ccf8b50eea0f
Kyle Hagler <khagler.kh@gmail.com>
2020-06-22 02:42:10 -0500

    final commit for now

--- redux-feedback-loop-master/src/components/Submission/Submission.js
+++ redux-feedback-loop-master/src/components/Submission/Submission.js
@@ -1,22 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import { Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 
 class Submission extends React.Component {
 
   redirect = () => {
     // TODO: Clear the cart and navigate to the product page
-    
-this.props.history.push('/'); //first stretch goal
-}
+
+    this.props.history.push('/'); //first stretch goal
+  }
   render() {
-  
+
     return (
       <>
-        <h2>Thank you!</h2> <br/>
-        <Button variant = "contained" color = "primary" onClick={this.redirect}>Leave New Feedback</Button>
+        <h2>Thank you!</h2> <br />
+        <Button variant="contained" color="primary" onClick={this.redirect}>Leave New Feedback</Button>
       </>
     ); // end return
   } // end render
